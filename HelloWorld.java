2ba7082866c46c4d6d0c374c8dfa5b04c43fb53e
suriyadav2009 <54509493+suriyadav2009@users.noreply.github.com>
2022-08-23 05:46:38 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,6 +34,7 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    System.out.println("Hello, World")
+      System.out.println("Good Evening");
   }
 }
